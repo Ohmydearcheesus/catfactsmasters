b769ae3fe8c743e0c302b7ce2510cf26385f2ce1
Ohmydearcheesus <ohmydearcheesus@gmail.com>
2019-08-25 00:19:13 -0700

    updated css and html after setup. pseudo coded JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// Where the questions and answers live (~20?)
+class Question {
+    constructor()
+}
+
+
+// Global variables
+
+// When the DOM loads....
+// Show the title screen, and offer pressing anywhere on the screen to start the game
+// When clicked, game starts
+// The first question appears, and the timer starts
+// User has 10s to select an answer by clicking on it
+//-----> if correct, add to their correct answers count
+//-----> show the correct answer + some extra explanation
+// At the end of the 10 questions quiz, show player their score and congratulate.
+// Offer to restart the game by pressing a "start over" button
+
+// Maybe make each question an object, and have them be added to an array?
+// Remove object when it has been used, and so that it cannot be called a second time
